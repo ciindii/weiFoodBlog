8ca2f3a333d49a9ff004aa1cc71f6964d92f7a6a
ciindii <cindythitruong@gmail.com>
2018-11-09 23:46:51 -0800

    added toggle nav JS

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Toggles hide/show nav
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches('.togglebtn')) {
+
+    var dropdowns = document.getElementsByClassName('toggle-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+console.log();
